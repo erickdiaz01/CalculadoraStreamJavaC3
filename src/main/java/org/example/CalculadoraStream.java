06e38fa0406f930933b150df8419be26107c3cf0
erickdiaz01 <erdiazb@unal.edu.co>
2022-07-07 18:01:19 -0500

    Correccion de calculadora

--- src/main/java/org/example/CalculadoraStream.java
+++ src/main/java/org/example/CalculadoraStream.java
@@ -1,63 +1,128 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class CalculadoraStream {
     List<Integer> listaAOperar1 ; ;
     List<Integer> listaAOperar2 ;
+    Integer limite;
 
     public CalculadoraStream() {
         this.listaAOperar1=List.of(1,2,3,4,5,6);
-        this.listaAOperar2= List.of(7,8,9,10,11);
+        this.listaAOperar2= List.of(8,12,15,11,19);
+       this.limite = this.listaAOperar1.size()<this.listaAOperar2.size()?this.listaAOperar1.size():this.listaAOperar2.size();
     }
-public Integer sumarListas(){
-        Optional<Integer> reduceSumaLista1 = listaAOperar1.stream().reduce((acumulador, numeroSig)->acumulador+=numeroSig);
-        Optional<Integer> reduceSumaLista2 =listaAOperar2.stream().reduce((acumulador, numeroSig)->acumulador+=numeroSig);
+public void sumarListas(){
 
-    return reduceSumaLista1.get() + reduceSumaLista2.get();
+        if(listaAOperar1.size()==listaAOperar2.size()){
+            listaAOperar1.stream().map(numero->{
+                Integer indice = listaAOperar1.indexOf(numero);
+                return numero + listaAOperar2.get(indice);
+            }).forEach(numeroSumado->{
+                System.out.println("La suma por posicion de los dos numeros de las listas es : "+numeroSumado);
+            });
+        }else {
+            listaAOperar1.stream().limit(this.limite).map(numero->{
+                Integer indice = listaAOperar1.indexOf(numero);
+                return numero + listaAOperar2.get(indice);
+            }).forEach(numeroSumado->{
+                System.out.println("La suma por posicion de los dos numeros de las listas es : "+numeroSumado);
+            });
+        }
 }
 
-public Integer restarListas(){
-    Optional<Integer> reduceRestaLista1 = listaAOperar1.stream().reduce((acumulador, numeroSig)->acumulador-=numeroSig);
-    Optional<Integer> reduceRestaLista2 = listaAOperar2.stream().reduce((acumulador, numeroSig)->acumulador-=numeroSig);
-    return reduceRestaLista1.get()+reduceRestaLista2.get();
+public void restarListas(){
+    if(listaAOperar1.size()==listaAOperar2.size()){
+        listaAOperar1.stream().map(numero->{
+            Integer indice = listaAOperar1.indexOf(numero);
+            return numero - listaAOperar2.get(indice);
+        }).forEach(numeroRestado->{
+            System.out.println("La suma por posicion de los dos numeros de las listas es : "+numeroRestado);
+        });
+    }else {
+        listaAOperar1.stream().limit(this.limite).map(numero->{
+            Integer indice = listaAOperar1.indexOf(numero);
+            return numero - listaAOperar2.get(indice);
+        }).forEach(numeroRestado->{
+            System.out.println("La resta por posicion de los dos numeros de las listas es : "+numeroRestado);
+        });
+    }
 
 }
-public Integer multiplicarSumaDeLasListas(){
-    Optional<Integer> reduceSumaLista1 = listaAOperar1.stream().reduce((acumulador, numeroSig)->acumulador+=numeroSig);
-    Optional<Integer> reduceSumaLista2 =listaAOperar2.stream().reduce((acumulador, numeroSig)->acumulador+=numeroSig);
-
-    return reduceSumaLista1.get()*reduceSumaLista2.get();
+public void multiplicarLasListas(){
+    if(listaAOperar1.size()==listaAOperar2.size()){
+        listaAOperar1.stream().map(numero->{
+            Integer indice = listaAOperar1.indexOf(numero);
+            return numero * listaAOperar2.get(indice);
+        }).forEach(producto->{
+            System.out.println("La suma por posicion de los dos numeros de las listas es : "+producto);
+        });
+    }else {
+        listaAOperar1.stream().limit(this.limite).map(numero->{
+            Integer indice = listaAOperar1.indexOf(numero);
+            return numero * listaAOperar2.get(indice);
+        }).forEach(producto->{
+            System.out.println("El producto por posicion de los dos numeros de las listas es : "+producto);
+        });
+    }
 }
 
-    public Integer multiplicarRestaDeLasListas(){
-        Optional<Integer> reduceRestaLista1 = listaAOperar1.stream().reduce((acumulador, numeroSig)->acumulador-=numeroSig);
-        Optional<Integer> reduceRestaLista2 = listaAOperar2.stream().reduce((acumulador, numeroSig)->acumulador-=numeroSig);
 
-        return reduceRestaLista1.get()*reduceRestaLista2.get();
-    }
-    public float dividirSumaDeLasListaConSkip(){
-        Optional<Integer> reduceSumaLista1 = listaAOperar1.stream().skip(2).reduce((acumulador, numeroSig)->acumulador+=numeroSig);
-        Optional<Integer> reduceSumaLista2 =listaAOperar2.stream().skip(2).reduce((acumulador, numeroSig)->acumulador+=numeroSig);
-        return reduceSumaLista1.get()/reduceSumaLista2.get();
-    }
-    public float dividirRestaDeLasListaConSkip(){
-        Optional<Integer> reduceRestaLista1 = listaAOperar1.stream().skip(3).reduce((acumulador, numeroSig)->acumulador-=numeroSig);
-        Optional<Integer> reduceRestaLista2 = listaAOperar2.stream().skip(3).reduce((acumulador, numeroSig)->acumulador-=numeroSig);
-        return reduceRestaLista1.get()/reduceRestaLista2.get();
-    }
-    public double potenciaMaximosDeListas(){
-        Integer maximoLista1= listaAOperar1.stream().max(Integer::compare).get();
-        Integer maximoLista2= listaAOperar2.stream().max(Integer::compare).get();
 
-        return Math.pow(maximoLista1,maximoLista2);
+    public void dividirListas(){
 
+        if(listaAOperar1.size()==listaAOperar2.size()){
+            listaAOperar1.stream().map(numero->{
+                Integer indice = listaAOperar1.indexOf(numero);
+                String condicion= (listaAOperar2.get(indice) != 0) ?String.valueOf((float)(numero * (Math.pow(listaAOperar2.get(indice),-1)))): "No es posible dividir por 0" ;
+                return condicion;            }).forEach(cociente->{
+                System.out.println("La division por posicion de los dos numeros de las listas es : "+cociente);
+            });
+        }else {
+            listaAOperar1.stream().limit(this.limite).map(numero->{
+                Integer indice = listaAOperar1.indexOf(numero);
+                String condicion= (listaAOperar2.get(indice) != 0) ?String.valueOf((float)(numero * (Math.pow(listaAOperar2.get(indice),-1)))): "No es posible dividir por 0" ;
+                return condicion;
+            }).forEach(cociente->{
+                System.out.println("La division por posicion de los dos numeros de las listas es : "+cociente);
+            });
+        }
     }
-    public Integer moduloMinimosDeListas(){
-        Optional<Integer> minimoLista1= listaAOperar1.stream().min(Integer::compare);
-        Optional<Integer> minimoLista2= listaAOperar2.stream().min(Integer::compare);
-        return minimoLista1.get()%minimoLista2.get();
+    public void potenciaDeListasConFilter(){
+        if(listaAOperar1.size()==listaAOperar2.size()){
+            listaAOperar1.stream().filter(numeroAFiltrar->numeroAFiltrar<3).map(numero->{
+                Integer indice = listaAOperar1.indexOf(numero);
+                return Math.pow(numero,listaAOperar2.get(indice));
+            }).forEach(potencia->{
+                System.out.println("La potencia por posicion de los dos numeros de las listas es : "+potencia);
+            });
+        }else {
+            listaAOperar1.stream().limit(this.limite).map(numero->{
+                Integer indice = listaAOperar1.indexOf(numero);
+                return Math.pow(numero,listaAOperar2.get(indice));
+            }).forEach(potencia->{
+                System.out.println("La potencia por posicion de los dos numeros de las listas es : "+potencia);
+            });
+        }
+    }
+    public void moduloSkipLastListas(){
+        if(listaAOperar1.size()==listaAOperar2.size()){
+            listaAOperar1.stream().filter(numeroAFiltrar->numeroAFiltrar<3).map(numero->{
+                Integer indice = listaAOperar1.indexOf(numero);
+                return numero%listaAOperar2.get(indice);
+            }).forEach(modulo->{
+                System.out.println("El modulo por posicion de los dos numeros de las listas es : "+modulo);
+            });
+        }else {
+            listaAOperar1.stream().limit(this.limite).map(numero->{
+                Integer indice = listaAOperar1.indexOf(numero);
+                return numero%listaAOperar2.get(indice);
+            }).forEach(modulo->{
+                System.out.println("El modulo por posicion de los dos numeros de las listas es : "+modulo);
+            });
+        }
     }
 
     }
